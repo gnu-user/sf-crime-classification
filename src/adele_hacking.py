320413638b53d1a6defc0f0b24da998d06645991
adelehedrick <adele@hedrick.ca>
2015-11-14 11:01:41 -0500

    Able to train so far with no errors, but takes lots of time!

--- src/adele_hacking.py
+++ src/adele_hacking.py
@@ -1,120 +1,81 @@
 import numpy as np 
 import pandas as pd
-from sklearn import svm, grid_search
-from sklearn.cross_validation import KFold
-import csv
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 from datetime import datetime, date
 
-print("Reading in training data...")
+print("Reading in training data")
 # raw_train = np.array(list(csv.reader(open("../data/train.csv", "rb"), delimiter=',')))
 raw_train = pd.read_csv('../data/train.csv')
-print("done")
 
-print("Reading in test data...")
+print("Reading in test data")
 # raw_test = np.array(list(csv.reader(open("../data/test.csv", "rb"), delimiter=',')))
 raw_test = pd.read_csv('../data/test.csv')
-print("done")
+
+print("Removing unused columns")
+raw_train.drop('Descript', axis=1, inplace=True)
+raw_train.drop('PdDistrict', axis=1, inplace=True)
+raw_train.drop('Resolution', axis=1, inplace=True)
+raw_train.drop('Address', axis=1, inplace=True)
+raw_test.drop('PdDistrict', axis=1, inplace=True)
+raw_test.drop('Address', axis=1, inplace=True)
 
 # Bin the GPS coordinates to ~100m radius precision
+print("Binning GPS coordinates")
 raw_train.X, raw_train.Y = raw_train.X.round(decimals=3), raw_train.X.round(decimals=3)
 raw_test.X, raw_test.Y = raw_test.X.round(decimals=3), raw_test.X.round(decimals=3)
 
 # Map day of week to integers from 1 - 7
-raw_train.DayOfWeek = raw_train.DayOfWeek.astype('category')
-raw_train.Category = raw_train.Category.astype('category')
-raw_test.DayOfWeek = raw_test.DayOfWeek.astype('category')
+print("Mapping day of the week")
+days_of_week = list(set(raw_train.DayOfWeek))
+days_of_week_dict = {days_of_week[x]: float(x) for x in range(len(days_of_week))}
+raw_train.DayOfWeek = raw_train.DayOfWeek.map(days_of_week_dict)
+raw_test.DayOfWeek = raw_test.DayOfWeek.map(days_of_week_dict)
 
-# Get the size of the long and lat bins
-print "TRAIN X bin: %d, Y bin: %d" % (len(raw_train.X.unique()), len(raw_train.Y.unique())) 
+print("Mapping categories")
+
+categories = list(set(raw_train.Category))
+categories_dict = {categories[x]: float(x) for x in range(len(categories))}
+raw_train_category = raw_train.Category.map(categories_dict)
 
 # Add columns to pandas for year, month, hour
+print("Extracting year, month, hour to separate columns within training (long process)")
 raw_train['Year'] = raw_train.Dates.map(lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S').year)
 raw_train['Month'] = raw_train.Dates.map(lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S').month)
 raw_train['Hour'] = raw_train.Dates.map(lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S').hour)
 
+print("Extracting year, month, hour to separate columns within test (long process)")
 raw_test['Year'] = raw_test.Dates.map(lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S').year)
 raw_test['Month'] = raw_test.Dates.map(lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S').month)
 raw_test['Hour'] = raw_test.Dates.map(lambda date: datetime.strptime(date, '%Y-%m-%d %H:%M:%S').hour)
 
 # Delete unnecessary data columns, training should match test columns
-# Dates, Descript, PdDistrict, Resolution, Address
+print("Deleting unnecessary columns out of training")
 raw_train.drop('Dates', axis=1, inplace=True)
-raw_train.drop('Descript', axis=1, inplace=True)
-raw_train.drop('PdDistrict', axis=1, inplace=True)
-raw_train.drop('Resolution', axis=1, inplace=True)
-raw_train.drop('Address', axis=1, inplace=True)
-
-raw_test.drop('Dates', axis=1, inplace=True)
-raw_test.drop('Dates', axis=1, inplace=True)
-raw_test.drop('Dates', axis=1, inplace=True)
-
-
-# Bin the data by year, month, hour
-
-# ['Id' 'Dates' 'DayOfWeek' 'PdDistrict' 'Address' 'X' 'Y']
-#print(raw_test[0])
-
-# Header of file: Dates,Category,Descript,DayOfWeek,PdDistrict,Resolution,Address,X,Y
-# Example contents: 015-05-13 23:53:00,WARRANTS,WARRANT ARREST,Wednesday,NORTHERN,"ARREST, BOOKED",
-# OAK ST / LAGUNA ST,-122.425891675136,37.7745985956747
+raw_train.drop('Category', axis=1, inplace=True)
 
 # extract test id column
+print("Extracting Id from test data")
 test_id = raw_test['Id']
 
-# create array of unique categories of crime
-print("Finding unique categories")
-categories = list(set(raw_train.Category))
-print(categories)
+print("Deleting unnecessary columns out of test")
+raw_test.drop('Dates', axis=1, inplace=True)
+raw_test.drop('Id', axis=1, inplace=True)
 
-# create array of unique dates in order
-print("Finding unique dates")
-train_dates = np.array([datetime.strptime(date, '%Y-%m-%d %H:%M:%S') for date in raw_train.Dates])
-test_dates = np.array([datetime.strptime(date, '%Y-%m-%d %H:%M:%S') for date in raw_test.Dates])
-dates = list(set(train_dates))
-print(dates)
+print("Casting data")
+columns = ['Year', 'Month', 'Hour', 'DayOfWeek', 'X', 'Y']
+np_train = np.array(raw_train[columns]).astype(float)
+np_train_category = np.array(raw_train_category).astype(float)
+np_test = np.array(raw_test[columns]).astype(float)
 
-# create array of unique days of week in order
-print("Finding unique days of the week")
-days_of_week = list(set(raw_train['DayOfWeek']))
+print("Training")
+clf = svm.SVC()
+clf.fit(np_train, np_train_category)
 
-# X and Y are already float types, so leave them be
+print("Predicting")
+test_classification = clf.predict(np_test)
 
-# create data frame representing the training data with data being converted to respective
-# index in the corresponding array
-print("Creating training data frame")
-train_df_data = {'Dates': (dates.index(row) for row in train_dates),
-                 'DaysOfWeek': (days_of_week.index(row) for row in raw_train['DayOfWeek']),
-                 'X': raw_train['X'],
-                 'Y': raw_train['Y']}
-train_df = pd.DataFrame(data=train_df_data, index=None)
+print("Saving to csv")
+np.savetxt('../results/sf-crime-submission.csv', test_classification, fmt='%f', delimiter=',')
 
-# create data frame representing the test data with data being converted to respective
-# index in the corresponding array
-print("Creating test data frame")
-test_df_data = {'Dates': (dates.index(row) for row in test_dates),
-                'DaysOfWeek': (days_of_week.index(row) for row in raw_test['DayOfWeek']),
-                'X': raw_test['X'],
-                'Y': raw_test['Y']}
-test_df = pd.DataFrame(data=test_df_data, index=None)
+print("Done!")
 
-
-scores = []
-k_fold_train = []
-k_fold = KFold(n=len(raw_train), n_folds=3)
-
-svc = svm.SVC()
-gammas = {x: categories[x] for x in len(categories)}
-clf = grid_search.GridSearchCV(estimator=svc, param_grid=dict(gamma=gammas), n_jobs=1)
-
-train_class = (categories.index(row) for row in raw_train['Category'])
-
-for k, (train, test) in enumerate(k_fold):
-    print("Fold on k= {0} working.".format(k))
-    clf.fit(train_df[train], train_class[train])
-    scores.append(clf.score(train_df[test], train_class[test]))
-    # k_fold_train.append(train)
-
-
-
-print("K-Folds best score: {0}".format(np.amax(scores)))
